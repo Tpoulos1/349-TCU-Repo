e0462a177fa29c6da6bd2096ee13dcde635e7acb
Tpoulos1 <theoapoulos@gmail.com>
2026-04-08 09:36:35 -0400

    Feat- Setup pins for SPI,GPIO, Sync in sync out

--- TCU.ino
+++ TCU.ino
@@ -1,2 +1,25 @@
-// Start of TCU code
-// This is a test line
+// TCU Project
+
+#include <SPI.h>
+
+// define specific pin numbers later &&
+
+#define SPI1 1// Serial clock
+#define SPI2 2// Master out Slave in
+#define SPI3 3// Master in Slave out
+
+#define GPIO 4// Sync pin for SPI
+
+#define SyncIN 5
+#define SyncOut 6
+
+
+
+void setup(){
+
+    Serial.begin(38400); // baud rate subject to change &&
+
+
+
+
+}
